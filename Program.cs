3d5d661a57cd3166e7149e1fe40d767e05a62373
KIRKA1356 <kgritsun@mail.ru>
2022-07-19 02:28:00 +0300

    over 19.07

---
+++ Program.cs
@@ -0,0 +1,197 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+1 2 4 7
+2 3 5 9
+2 4 4 8
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка
+Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
+Например, заданы 2 массива:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+и
+1 5 8 5
+4 9 4 2
+7 2 2 6
+2 3 4 7
+Их произведение будет равно следующему массиву:
+1 20 56 10
+20 81 8 6
+56 8 4 24
+10 6 24 49
+Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+массив размером 2 x 2 x 2
+12(0,0,0) 22(0,0,1)
+45(1,0,0) 53(1,0,1)
+Задача 62. Заполните спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+1 2 3 4
+12 13 14 5
+11 16 15 6
+10 9 8 7
+*/
+
+/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+1 2 4 7
+2 3 5 9
+2 4 4 8
+*/
+
+/*
+Console.WriteLine("Введите значение строк");
+int rows = Convert.ToInt32(Console.ReadLine());                     //Задача 54
+Console.WriteLine("Введите значение столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение массива");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение массива");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateRandom2dArray(int rows, int columns, int min, int max)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    Console.WriteLine("Задан массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int[,] Uporiadochit(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int temp = array[i, j];
+            if (j==1)
+            {
+                j=0;
+                if (array[i, j] > array[i, j + 1])
+                {
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                    j=2;
+                }
+            }
+            if (array[i, j] > array[i, j + 1])
+            {
+                array[i, j] = array[i, j + 1];
+                array[i, j + 1] = temp;
+            }
+        }
+
+    }
+    return array;
+}
+
+int[,] MyArray = CreateRandom2dArray(rows, columns, min, max);
+Show2dArray(MyArray);
+Show2dArray(Uporiadochit(MyArray));
+*/
+
+Console.WriteLine("Введите значение строк");
+int rows = Convert.ToInt32(Console.ReadLine());                     //Задача 56
+Console.WriteLine("Введите значение столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение массива");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение массива");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateRandom2dArray(int rows, int columns, int min, int max)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    Console.WriteLine("Задан массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int NaimSum(int[,] array)
+{
+    int min = 1000;
+    int min_indx = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+            sum = array[i, j] + sum;
+            }
+        if (min > sum)
+        {
+            min = sum;
+            min_indx = i;
+            sum = 0;
+        }
+    }
+    return min_indx;
+}
+
+int[,] MyArray = CreateRandom2dArray(rows, columns, min, max);
+Show2dArray(MyArray);
+
+Console.WriteLine("Индекс строки с наименьшой суммой элементов: " + NaimSum(MyArray));
